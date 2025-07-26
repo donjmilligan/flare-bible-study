b7737bee2e1fea3323244401ba145fd8e7ece112
Bereket <bereketwubalem29@gmail.com>
2025-07-26 18:11:01 +0300

    feat: improve  the editable features for message of hope

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import OldTestamentJesus1 from "./components/oldtestamentjesus1/OldTestamentJesus1";
 import OldTestamentJesus2 from "./components/oldtestamentjesus2/OldTestamentJesus2";
+import MessageofHope from "./components/messageofhope/MessageofHope";
 import Dashboard from "./components/dashboard/Dashboard";
 import "./App.css";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/jesus1" element={<OldTestamentJesus1 />} />
         <Route path="/jesus2" element={<OldTestamentJesus2 />} />
+        <Route path="/messageofhope" element={<MessageofHope />} />
       </Routes>
     </Layout>
   );
